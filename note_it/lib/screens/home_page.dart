2102c03bf7b8cab1035205b9c9b30a31d4d48174
MartienJun <martienjunaedi123@gmail.com>
2020-10-01 12:45:35 +0700

    feat: change app icon
    add (folder):
    1. icon (root -> assets -> icons)
    
    add (file):
    1. Icon_note_it

--- note_it/lib/screens/home_page.dart
+++ note_it/lib/screens/home_page.dart
@@ -12,13 +12,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          'Note It',
-          style: TextStyle(
-            fontFamily: 'Montserrat',
-            fontWeight: FontWeight.w700,
-          ),
-        ),
+        backgroundColor: Color.fromRGBO(250, 225, 71, 1),
       ),
     );
   }
